c44fdd94e0a2de077e3c530ec177410b41ddd453
Aleksand Sorokin <frogsav@yandex.ru>
2019-10-09 21:21:49 +0300

    Вставка данных в таблицу и возврат результата

--- adp2.php
+++ adp2.php
@@ -3,12 +3,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 use phpseclib\Crypt\RSA;
 use App\Test\Helpers;
+use App\Test\Crud;
 
 $json = file_get_contents('php://input');
 $json = json_decode($json,true);
 if (!$json) {
     header("HTTP/1.0 404 Not Found");
     header('Content-Type: application/json');
+
     die(json_encode(["status" => "error", "description" => "no data send"]));
 }
 
@@ -21,8 +23,16 @@ $decriptRequestData = $helpers->getDecriptData();
 $verify = $helpers->verifySignature($decriptRequestData);
 
 if($verify){
+    $decriptRequestData = unserialize($decriptRequestData);
     //insert user into database
-    die(json_encode(["status" => "success", "description" => "Спасибо, Ваши данные приняты"]));
+    $crud = new Crud();
+    $res = $crud->insert('wallet_user111',$decriptRequestData);
+    if($res=='success'){
+    die(json_encode(["status" => "success", "description" => "Спасибо, Ваши данные приняты",
+        "data"=>$decriptRequestData]));
+    }else{
+        die(json_encode(["status" => "error", "description" => "Транзакция неуспешна повторите позже попытку "]));
+    }
 }else{
     die(json_encode(["status" => "error", "description" => "Ваши данные испорчены"]));
 }
---
+++ src/Crud.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Test;
+use Doctrine\DBAL\DriverManager;
+
+class Crud
+{
+public $connectionParams = array('dbname' => 'data',
+                                'user' => 'postgres',
+                                'password' => '',
+                                'host' => '127.0.0.1',
+                                'port ' =>'5432',
+                                'driver' => 'pdo_pgsql');
+public $conn;
+public function __construct()
+{
+    $config = new \Doctrine\DBAL\Configuration();
+    return $this->conn = DriverManager::getConnection($this->connectionParams, $config);
+}
+
+public function insert($tableName,array $arrayData){
+    $this->conn->beginTransaction();
+    try{
+        $this->conn->insert($tableName,$arrayData);
+        $this->conn->commit();
+        return 'success';
+    } catch (\Exception $e) {
+        $this->conn->rollBack();
+        return $e->getMessage();
+    }
+}
+
+}
